cd9e0521300eb1d6c2c83526e5398a5f9512166f
Shubhangi Naik <shubhanginaik@Shubhangis-MacBook-Air.local>
2024-09-29 16:01:21 +0300

    added components and containers basic structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from "react";
 
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { Brand, ACT, Navbar } from "./components";
+
 const App = () => {
   return (
-    <div>
+    <div className="App">
+      <div>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+
       <h1>App</h1>
     </div>
   );
