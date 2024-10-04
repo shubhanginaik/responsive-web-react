16be40885b94fc29dc6e6c8f6a1cba7048115c09
Shubhangi Naik <shubhangi.naik83@gmail.com>
2024-10-04 18:25:42 +0300

    updated header css

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
           <button>Get Started</button>
         </div>
         <div className="gpt3__header-content_people">
-          <img src={people} alt="people.image" />
+          <img src={people} alt="people-image" />
           <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
       </div>
