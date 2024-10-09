3b7f047412aefc16b04293eef4fb0a69e351b8c0
Shubhangi Naik <shubhangi.naik83@gmail.com>
2024-10-09 21:16:28 +0300

    added Feature container

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./features.css";
+import { Feature } from "../../components";
+
+const featureData = [
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+  {
+    title: "Improving end distrusts instantly ",
+    text: "From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.",
+  },
+];
+
+const Features = () => {
+  return (
+    <div className="gpt3__features section__padding" id="features">
+      <div className="gpt3__features_heading">
+        <h1 className="gradient__text">
+          The Future is Now and You Just Need To Realize It. Step into Future
+          Today & Make it Happen.
+        </h1>
+        <p>Request Early Access to Get Started</p>
+      </div>
+      <div className="gpt3__features-container">
+        {featureData.map((item, index) => (
+          <Feature
+            title={item.title}
+            text={item.text}
+            key={item.title + index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Features;
